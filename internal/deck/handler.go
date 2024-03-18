e6b51a2414eb48086402fbad6a09c77b26d5ea02
Joao <jvfr96@gmail.com>
2024-03-18 12:35:33 -0300

    feat: add options and repository to handler

--- internal/deck/handler.go
+++ internal/deck/handler.go
@@ -4,6 +4,7 @@ package deck
 import (
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/jvfrodrigues/deck-api/internal/env"
 	"github.com/labstack/echo/v4"
 	"github.com/sirupsen/logrus"
@@ -11,8 +12,9 @@ import (
 
 // Handler for deck endpoints
 type Handler struct {
-	logger *logrus.Logger
-	env    *env.Vars
+	logger     *logrus.Logger
+	env        *env.Vars
+	repository *DeckRepository
 }
 
 // UseSubroute registers the deck routes
@@ -20,9 +22,26 @@ func (h *Handler) UseSubroute(group *echo.Group) {
 	group.POST("/", h.createDeck)
 }
 
+type createDeckResponse struct {
+	ID        uuid.UUID `json:"deck_id"`
+	Shuffled  bool      `json:"shuffled"`
+	Remaining int       `json:"remaining"`
+}
+
 func (h *Handler) createDeck(c echo.Context) error {
-	deck := NewDeck(false)
-	return c.JSON(http.StatusOK, deck)
+	shuffled := c.QueryParam("shuffled") == "true"
+	cards := c.QueryParam("cards")
+	deck := NewDeck(Shuffled(shuffled), Partial(cards))
+	err := h.repository.Save(*deck)
+	if err != nil {
+		return err
+	}
+	response := &createDeckResponse{
+		ID:        deck.ID,
+		Shuffled:  deck.Shuffled,
+		Remaining: len(deck.Cards),
+	}
+	return c.JSON(http.StatusOK, response)
 }
 
 // NewHandler returns a new instance of a deck handler
@@ -30,8 +49,10 @@ func NewHandler(
 	env *env.Vars,
 	logger *logrus.Logger,
 ) *Handler {
+	repository := NewRepository()
 	return &Handler{
-		env:    env,
-		logger: logger,
+		env:        env,
+		logger:     logger,
+		repository: repository,
 	}
 }
