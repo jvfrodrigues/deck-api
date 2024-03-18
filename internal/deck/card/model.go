1e35fb328e3307752df3d387b18aa3c3222e8dd2
Joao <jvfr96@gmail.com>
2024-03-18 14:56:57 -0300

    chore: add comments

--- internal/deck/card/model.go
+++ internal/deck/card/model.go
@@ -1,5 +1,7 @@
+// package card has all card related requirements
 package card
 
+// type Card represents the structure of a card
 type Card struct {
 	Value string `json:"value"`
 	Suit  string `json:"suit"`
