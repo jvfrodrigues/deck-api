8eca6a59746b23436bee6a991f03ee3086bbb72f
Joao <jvfr96@gmail.com>
2024-03-26 10:02:36 -0300

    refactor: use map on repository instead of slice

--- internal/deck/repository.go
+++ internal/deck/repository.go
@@ -3,51 +3,43 @@ package deck
 import (
 	"errors"
 
+	"github.com/google/uuid"
 	"github.com/jvfrodrigues/deck-api/internal/deck/card"
 )
 
 // DeckRepository has the actual implementation of the deck repository
 type DeckRepository struct {
-	decks []Deck
+	decks map[string]Deck
 }
 
 // NewRepository creates a new DeckRepository
 func NewRepository() *DeckRepository {
 	return &DeckRepository{
-		decks: make([]Deck, 0),
+		decks: make(map[string]Deck),
 	}
 }
 
 // Save adds a new deck to the DeckRepository
 func (r *DeckRepository) Save(deck Deck) error {
-	r.decks = append(r.decks, deck)
+	r.decks[deck.ID.String()] = deck
 	return nil
 }
 
 // Get gets a deck from the DeckRepository by its ID
 func (r *DeckRepository) Get(id string) (*Deck, error) {
-	var foundDeck *Deck
-	for _, deck := range r.decks {
-		if deck.ID.String() == id {
-			foundDeck = &deck
-		}
-	}
-	if foundDeck == nil {
+	var foundDeck Deck
+	foundDeck = r.decks[id]
+	if foundDeck.ID == uuid.Nil {
 		return nil, errors.New("deck not found")
 	}
-	return foundDeck, nil
+	return &foundDeck, nil
 }
 
 // Draw gets an existing deck from the DeckRepository and draws the number of cards passed
 func (r *DeckRepository) Draw(id string, count int) ([]card.Card, error) {
-	var foundDeck *Deck
-	for i, deck := range r.decks {
-		if deck.ID.String() == id {
-			foundDeck = &r.decks[i]
-			break
-		}
-	}
-	if foundDeck == nil {
+	var foundDeck Deck
+	foundDeck = r.decks[id]
+	if foundDeck.ID == uuid.Nil {
 		return nil, errors.New("deck not found")
 	}
 	if count > len(foundDeck.Cards) {
@@ -59,5 +51,6 @@ func (r *DeckRepository) Draw(id string, count int) ([]card.Card, error) {
 		drawnCards[i] = foundDeck.Cards[0]
 		foundDeck.Cards = foundDeck.Cards[1:]
 	}
+	r.decks[id] = foundDeck
 	return drawnCards, nil
 }
